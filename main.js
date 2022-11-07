47518091e0f0f009127482059862a22148edd974
Meint Local <mwverhoeff+vscode@gmail.com>
2022-11-07 16:31:19 +0100

    update logic updated index.html, main (js and css)

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const txtNext = document.querySelector('.next');
 const ridersList = document.getElementById('ridersList');
 let rider = 0 //starting position
 let numOfRiders = 8;
-var riders = {};
+var riders = [];
 let myIntervalTimer = 0; //in seconds
 var myInterval = null; //variable for setinterval object
 
 function createRiderList () {
-    rdrs = {};
-    for (let i = 0; i <8; i++) {
-        rdrs[i] = {RiderId:i, Name: "Rider"+(i+1), Interval:60, Active:true};
-    }
-    return rdrs;
+    var rdrs = [...Array(numOfRiders).keys()].map(function(item) {
+        return { RiderId : item, RiderName: `Rider${item+1}`, Interval : 60, Active: 'Yes' };
+    });
+   return rdrs;
+    
 }
 
 //store rider object To LocalStorage
@@ -30,31 +30,6 @@ function getRidersFLS() {
     obj = JSON.parse(localStorage.getItem('data'));
     return obj;
 }
-
-function updateActiveRiders() {
-    //only accept input from 3 till 8 and no characters  
-    let i = parseInt($('.numberOfRiders tr div').html());
-    if (!(isNaN(i) || ( i < 3 ) || (i > 8))) {
-        console.log(rider, numOfRiders);
-        if ((rider % numOfRiders == 0) && (rider > 0)) {
-            rider = numOfRiders;
-        } 
-        else
-        { 
-            rider = rider % numOfRiders;
-            numOfRiders = i;
-            
-        }
-        console.log(rider , i, numOfRiders);
-        createTableofRiders();
-        
-    }
-    $('.numberOfRiders tr div').html(numOfRiders);
-    //createTableofRiders();  
-    
-}
-
-
 
 btnStartNext.click(function() {
     start();
@@ -69,14 +44,32 @@ $(document).on('click', '.row_data', function(event) {
     if($(this).attr('edit_type') == 'button') {
         return false;
     }
+    if($(this).attr('col_name') == 'Pull') {
+        var rowId = $(this).closest('tr').attr('rowId'); //reads value of attribute rowId;
+        var rowDiv = $(this);
+        if (rowDiv.html() == 'Yes') {
+            riders[riders.findIndex( (rdr) => rdr.RiderId == rowId)].Active = 'No';
+            rowDiv.html('No');
+        } else { 
+            riders[riders.findIndex( (rdr) => rdr.RiderId == rowId)].Active = 'Yes';
+            rowDiv.html('Yes');
+        };
+        pushRidersTLS(riders); //store update locally
+        createTableofRiders(); //update table on screen
+        return;
+    }
+
     $(this).closest('div').attr('contenteditable','true');
     $(this).addClass('bg-secondary').css('padding', '4px');
     $(this).focus();
     $(this).keypress( function(e) {
+        
         if($(this).attr('col_name') == 'Interval') {
+            console.log($(this).attr('col_name'));
             if(isNaN(String.fromCharCode(e.which))) e.preventDefault();
         };
     });
+    $(this)
 });
 
 $(document).on('focusout', '.row_data', function(event) {
@@ -87,16 +80,36 @@ $(document).on('focusout', '.row_data', function(event) {
 
     var rowId = $(this).closest('tr').attr('rowId');
     var row_div = $(this).removeClass('bg-secondary').css('padding','');
-
     var col_name = row_div.attr('col_name');
-    if (col_name == 'numOfRiders') {
-        updateActiveRiders();
+    var col_val = row_div.text();
+    //riders[rowId][col_name] = col_val;
+    if (col_name == 'Interval') {
+        //check input interval must be integer and larger then 10 other reset to old setting
+        if(isNumber(col_val)) {
+            if ( col_val > 10) {
+                //interval value in riders object
+                riders[riders.findIndex( (rdr) => rdr.RiderId == rowId)][col_name] = parseInt(col_val);
+            }
+        }
+        //reset screen value to existing setting           
+        row_div.html(riders[riders.findIndex( (rdr) => rdr.RiderId == rowId)][col_name]);
     } else {
-        var col_val = row_div.html();
-           riders[rowId][col_name] = col_val;
-           pushRidersTLS(riders);
+        //update for other columns then Interval - strings are allowed.
+        riders[riders.findIndex( (rdr) => rdr.RiderId == rowId)][col_name] = col_val; 
+    } 
+
+    
+    pushRidersTLS(riders);
+});
+
+function isNumber(s)
+    {
+        for (let i = 0; i < s.length; i++)
+            if (s[i] < '0' || s[i] > '9')
+                return false;
+  
+        return true;
     }
-})
 
 function timer() {
     myIntervalTimer--;
@@ -107,11 +120,14 @@ function timer() {
 function reset() {
     clearInterval(myInterval);
     myInterval = null;
-    rider = 0;
+    riders.sort((a,b)=> {
+        return a.RiderId - b.RiderId;
+    });
     btnStartNext.html('Start');
-    txtNext.innerText = (riders)[rider % numOfRiders].Name;
-    txtTimer.innerText = `--`;
+    txtNext.innerHTML = riders[0].RiderName;
+    txtTimer.innerText = riders[0].Interval;
     createTableofRiders();
+    console.log('from reset function :',riders)
 }
 
 function start() {
@@ -123,61 +139,47 @@ function start() {
 }
 
 function next() {
-    let nextRider = rider++ % numOfRiders;
-    myIntervalTimer = (riders)[nextRider].Interval;
-//    console.log((riders)[nextRider].RiderId);
-    txtNext.innerText = (riders)[nextRider].Name;
-    createTableofRiders();
+    if (riders[0].Active == 'Yes') {
+        myIntervalTimer = riders[0].Interval;
+        txtNext.innerHTML = riders[0].RiderName;
+        riders.push((riders.splice(0,1)[0]));
+        createTableofRiders();
+    } else { 
+        riders.push((riders.splice(0,1)[0]));
+        next(); //potential looping error!!!
+    }
 }
 
+function riderTableHeader () {
+    var tableStr ='<table class="table table-hover table-borderless">';
+    tableStr +='<thead><tr><th>Name</th><th>Interval</th><th>Pull</th></tr></thead>';
+    return tableStr;
+}
 
 function createTableofRiders(){
-    var riderTbl = '';
-    riderTbl +='<table class="table table-hover table-borderless">';
-
-    riderTbl +='<thead>';
-        riderTbl +='<tr>';
-        riderTbl +='<th>Name</th>';
-        riderTbl +='<th>Interval</th>';
-        riderTbl +='</tr>';
-    riderTbl +='</thead>';
-
-    riderTbl +='<tbody>';
-    console.log(riders);
-    $.each(riders, function(i){
-       var index = parseInt(i); 
-        if (index == numOfRiders) {
-            return false;
-        };
-        let idx = parseInt((rider + index) % numOfRiders);
-            riderTbl +=`<tr rowId="${riders[idx].RiderId}">`
-                riderTbl +=`<td><div class="row_data" edit_type="click" col_name="Name">${riders[idx].Name}</div></td>`;
-                riderTbl +=`<td><div class="row_data" edit_type="click" col_name="Interval">${riders[idx].Interval}</div></td>`;
-            });
-        
-        riderTbl +='</tr>';
-    riderTbl +='</tbody>';
-riderTbl +='</table>';
-$(document).find('.ridersTable').html(riderTbl);
+    console.log('creattable of riders');
+    var rdrTblAct = riderTableHeader();
+    var rdrTblNotAct = '<tr><td><div><hr></div></td><td><div><hr></div></td><td><div><hr></div></td><tr>' /*empty data row*/;
+    rdrTblAct +='<tbody>';
+   // numOfRiders = riders.filter(rider => rider.Active == 'Yes').length;
+    riders.forEach(rdr => {
+        if ( rdr.Active == 'Yes') {
+            rdrTblAct +=`<tr rowId="${rdr.RiderId}">`
+                rdrTblAct +=`<td><div class="row_data" edit_type="click" col_name="RiderName">${rdr.RiderName}</div></td>`;
+                rdrTblAct +=`<td><div class="row_data" edit_type="click" col_name="Interval">${rdr.Interval}</div></td>`;
+                rdrTblAct +=`<td><div class="row_data" edit_type="click" col_name="Pull">${rdr.Active}</div></td>`;
+            } else {
+            rdrTblNotAct +=`<tr rowId="${rdr.RiderId}">`
+                rdrTblNotAct +=`<td><div class="row_data bg-secondary" edit_type="click" col_name="RiderName">${rdr.RiderName}</div></td>`;
+                rdrTblNotAct +=`<td><div class="row_data bg-secondary" edit_type="click" col_name="Interval">${rdr.Interval}</div></td>`;
+                rdrTblNotAct +=`<td><div class="row_data bg-secondary" edit_type="click" col_name="Pull">${rdr.Active}</div></td>`;
+            }});
+        rdrTblAct += rdrTblNotAct;       
+        rdrTblAct +='</tr>';
+    rdrTblAct +='</tbody>';
+rdrTblAct +='</table>';
+$(document).find('.ridersTable').html(rdrTblAct);
 }
-
-function createTableNumberOfRiders() {
-    var riderTbl = '';
-    riderTbl +='<table class="table table-hover table-borderless">';
-    riderTbl +='<thead>';
-        riderTbl +='<tr>';
-        riderTbl +='<th>Number of Riders</th>';
-        riderTbl +='</tr>';
-    riderTbl +='</thead>';
-    riderTbl +='<tbody>';
-            riderTbl +=`<tr">`
-                riderTbl +=`<td><div class="row_data" edit_type="click" col_name="numOfRiders">8</div></td>`;
-        riderTbl +='</tr>';
-    riderTbl +='</tbody>';
-riderTbl +='</table>';
-$(document).find('.numberOfRiders').html(riderTbl);
-}
-
 
 function startOnload(){
     //Initialize RiderList
@@ -190,9 +192,10 @@ function startOnload(){
         //console.log(riders);
     }
     $.when( $.ready ).then(function () {
+        txtNext.innerText = riders[0].RiderName;
+        txtTimer.innerText = riders[0].Interval;
         createTableofRiders();
-        createTableNumberOfRiders()
     });   
 }
-
+//localStorage.clear();
 startOnload();
